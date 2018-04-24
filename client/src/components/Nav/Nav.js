90e65e77fa5cc9ade6763003edd4a5919ce96529
astie086 <astie086@gmail.com>
2018-04-24 14:50:39 -0700

    added best and worst folders and updated navbar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -5,19 +5,19 @@ const NavTabs = () => (
   <ul className="nav nav-tabs">
         
         <li className="nav-item">
-        <Link to="/blog" className={window.location.pathname === "/feed" ? "nav-link active" : "nav-link"}><Logo/></Link>
+        <Link to="/Logo" className={window.location.pathname === "/feed" ? "nav-link active" : "nav-link"}><Logo/></Link>
         </li>
         
         <li className="nav-item">
-        <Link to="/feed" className={window.location.pathname === "/feed" ? "nav-link active" : "nav-link"}> Feed</Link>
+        <Link to="/Feed" className={window.location.pathname === "/feed" ? "nav-link active" : "nav-link"}> Feed</Link>
         </li>
         
         <li className="nav-item">
-        <Link to= "/best" className={window.location.pathname === "/best" ? "nav-link active" : "nav-link"}>Best</Link>
+        <Link to= "/Best" className={window.location.pathname === "/best" ? "nav-link active" : "nav-link"}>Best</Link>
         </li>
         
         <li className="nav-item">
-        <Link to="/worst" className={window.location.pathname === "/worst" ? "nav-link active" : "nav-link"}>Worst</Link>
+        <Link to="/Worst" className={window.location.pathname === "/worst" ? "nav-link active" : "nav-link"}>Worst</Link>
         </li>
   </ul>
 );
