bf09de718cfae2582ae1ab7570ba3aea186dd498
astie086 <astie086@gmail.com>
2018-04-25 10:34:59 -0700

    update to user

--- client/src/components/User/User.js
+++ client/src/components/User/User.js
@@ -4,7 +4,12 @@ class User extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <h2>This is the user column</h2>
+         <div class="sidebar-header">
+            <img src="cinqueterre.jpg" class="img-circle" alt="Cinque Terre"/>
+        </div>
+        <h2>User Name</h2>
+        <div>Points <span class="badge badge-warning">3</span></div>
+        <div>Rank <span class="badge badge-warning">3</span></div>
       </div>
     )
   }
