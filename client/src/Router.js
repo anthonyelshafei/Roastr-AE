49c757c5566430ae5dec2513cb5620ab63726627
astie086 <astie086@gmail.com>
2018-04-25 14:27:59 -0700

    update to patrickstar image and router.js

--- client/src/Router.js
+++ client/src/Router.js
@@ -4,9 +4,7 @@ import Main from "../src/pages/Main"
 import Login from "../src/pages/Login";
 import SignUp from "../src/pages/SignUp";
 import NotFound from "../src/pages/NotFound";
-import Worst from "./components/Worst";
-import Best from "./components/Best";
-import Feed from "./components/Feed";
+
 
 const Router = () => (
   
@@ -15,10 +13,6 @@ const Router = () => (
           <Route exact path="/" component={Login}/>
           <Route exact path="/signup" component={SignUp}/>
           <Route path="/main" component={Main}/>
-          {/* <Switch>
-          <Route exact path="/main/Worst" component={Main}/>
-          <Route exact path="/main/Best" component={Main}/>
-          </Switch> */}
           <Route component={NotFound}/>
         </Switch>
   </BrowserRouter>
---
+++ client/src/components/Userimage/Userimage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Userimage = props => (
+  <div className="text-center col-lg-6 col-offset-lg-3 col-md-6 col-md-offset-3 col-sm-10 col-sm-offset-2">
+    <img src="https://res.cloudinary.com/teepublic/image/private/s--Tm_-OgRz--/t_Preview/b_rgb:0f7b47,c_limit,f_jpg,h_630,q_90,w_630/v1446161353/production/designs/71233_0.jpg" className="img-responsive" alt=""/>
+  </div>
+);
+
+export default Userimage;
+
+
