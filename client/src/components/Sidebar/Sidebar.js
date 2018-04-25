7817d5393dedd3db97c6c009ec3b31d842068564
astie086 <astie086@gmail.com>
2018-04-25 10:22:25 -0700

    sidebar update

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -7,17 +7,20 @@ class Sidebar extends React.Component {
     return (
 
 
-        <div class="wrapper">
+        <div class="container">
 
         <nav id="sidebar">
+        
             <div class="sidebar-header">
-            <img src="cinqueterre.jpg" class="img-circle" alt="Cinque Terre">
+            <img src="cinqueterre.jpg" class="img-circle" alt="Cinque Terre"/>
             </div>
 
-            <ul class="Stats">
-                <li><a href="#">Points</a></li>
-                <li><a href="#">Ranks</a></li>
-            </ul>
+            <h2>Username</h2>
+
+            <div class="Stats">
+                <div>Points <span class="badge badge-warning">3</span></div>
+                <div>Rank <span class="badge badge-warning">3</span></div>
+            </div>
         </nav>
 
         </div>
