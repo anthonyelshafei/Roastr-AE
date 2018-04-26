ec52dd1a9f8a6fe6959d3d0450b63a02dec32b49
Chulps <chuckoward@gmail.com>
2018-04-25 21:41:13 -0700

    fixed layout of completed roasts a bit

--- client/src/components/CompRoast/CompRoast.js
+++ client/src/components/CompRoast/CompRoast.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import Userimage from "../Userimage";
 
+const style = {
+    height: 60,
+    width: 60,
+  };
 
 class CompRoast extends React.Component {
     render() {
         return ( 
             
             <div className="card text-center">
-                <div className="card-header row text-center">
-                
-                    <div className="col-3"/>
+                <div className="card-header row text-center justify-content-between">
                     
-                    <div className="col-1">
+                    <div style={style} className="d-flex ">
                         <Userimage />
                     </div>
                     
-                    <div className="col-4 my-auto">
+                    <div className="d-flex my-auto">
                         Player 1 @ Player 2
                     </div>
                     
-                    <div className="col-1">
+                    <div style={style} className="d-flex">
                         <Userimage />
                     </div>
-
-                    <div className="col-3"/>
 
                 </div>
                 
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -9,7 +9,7 @@ const NavTabs = () => (
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
-  <div className="collapse navbar-collapse justify-content-between" id="navbarNav">
+  <div className="collapse navbar-collapse" id="navbarNav">
     <ul className="navbar-nav mx-auto">
             
       <li className="nav-item">
