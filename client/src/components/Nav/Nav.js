0e7a7e33959a06f13c684a75636e5113418f95de
Chulps <chuckoward@gmail.com>
2018-04-26 03:48:51 -0700

    fixed navbar and logo

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -2,42 +2,37 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const imgStyle = {
-  maxHeight: 80,
+  maxHeight: 130,
 };
 
-const linkStyle ={
-  color: "white",
-}
-
 // style={imgStyle}
-// style={linkStyle}
 
 const NavTabs = () => (
 
-<nav className="navbar navbar-expand-lg navbar-light sticky-top">
-  <a className="navbar-brand" href=""><img style={imgStyle} src="https://i.imgur.com/9t9gT3Y.png" alt=""/></a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
+<nav className="navbar navbar-expand-lg sticky-top p-0">
+  <a className="navbar-brand text-center ml-3" href=""><img style={imgStyle} src="https://i.imgur.com/9t9gT3Y.png" alt="Roastr"/></a>
+  <button className="navbar-toggler mr-3 bg-danger" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navbarNav">
-    <ul className="navbar-nav mx-auto text-center ">
+    <ul className="navbar-nav text-warning">
             
-      <li className="nav-item px-4">
-        <Link to="/main/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}> Feed</Link>
+      <li className="nav-item px-2">
+        <Link to="/main/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>Feed</Link>
       </li>
       
-      <li className="nav-item px-4">
+      <li className="nav-item px-2">
         <Link to= "/main/Best" className={window.location.pathname === "/best" ? "nav-link active" : "nav-link"}>Best</Link>
       </li>
       
-      <li className="nav-item px-4">
+      <li className="nav-item px-2">
         <Link to="/main/Worst" className={window.location.pathname === "/worst" ? "nav-link active" : "nav-link"}>Worst</Link>
       </li>
 
     </ul>
-    <div className="col-12 col-xs-12 col-lg-1 py-2 text-center border border-danger">
-      <Link to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>GTFO</Link>
-    </div>
+
+      <span className="btn col-12 col-xs-12 col-lg-1 py-2 mr-3 ml-auto">GTFO</span>
     
   </div>
 </nav>
--- client/src/components/User/User.js
+++ client/src/components/User/User.js
@@ -5,12 +5,13 @@ class User extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <h2 class = "text-center">User Name</h2>
          <div className="sidebar-header">
           <Userimage/>
+          <h2 class = "text-center">User Name</h2>
           </div>
         <div>Points <span className="badge badge-pill badge-warning float-right">3</span></div>
         <div>Rank <span className="badge badge-pill badge-warning float-right">3</span></div>
+        <br/>
       </div>
     )
   }
