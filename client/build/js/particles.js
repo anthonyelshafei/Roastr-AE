19d1d66f4604dc34fd577949f5dffc2a0ac4860b
Ian Miller <imiller1007@yahoo.com>
2018-04-27 15:26:58 -0700

    added build version

---
+++ client/build/js/particles.js
@@ -0,0 +1,110 @@
+particlesJS("particles-js", {
+	"particles": {
+		"number": {
+			"value": 53,
+			"density": {
+				"enable": true,
+				"value_area": 481.0236182596568
+			}
+		},
+		"color": {
+			"value": "#ffd700"
+		},
+		"shape": {
+			"type": "circle",
+			"stroke": {
+				"width": 2,
+				"color": "#ffa500"
+			},
+			"polygon": {
+				"nb_sides": 5
+			},
+			"image": {
+				"src": "img/github.svg",
+				"width": 100,
+				"height": 100
+			}
+		},
+		"opacity": {
+			"value": 1,
+			"random": true,
+			"anim": {
+				"enable": false,
+				"speed": 1,
+				"opacity_min": 0.1,
+				"sync": false
+			}
+		},
+		"size": {
+			"value": 2,
+			"random": true,
+			"anim": {
+				"enable": true,
+				"speed": 12.181158184520175,
+				"size_min": 0.1,
+				"sync": false
+			}
+		},
+		"line_linked": {
+			"enable": false,
+			"distance": 150,
+			"color": "#ffffff",
+			"opacity": 0.4,
+			"width": 1
+		},
+		"move": {
+			"enable": true,
+			"speed": 6,
+			"direction": "top-left",
+			"random": true,
+			"straight": false,
+			"out_mode": "out",
+			"bounce": false,
+			"attract": {
+				"enable": true,
+				"rotateX": 1657.2100474277727,
+				"rotateY": 789.1476416322727
+			}
+		}
+	},
+	"interactivity": {
+		"detect_on": "canvas",
+		"events": {
+			"onhover": {
+				"enable": true,
+				"mode": "repulse"
+			},
+			"onclick": {
+				"enable": false,
+				"mode": "remove"
+			},
+			"resize": true
+		},
+		"modes": {
+			"grab": {
+				"distance": 400,
+				"line_linked": {
+					"opacity": 1
+				}
+			},
+			"bubble": {
+				"distance": 400,
+				"size": 40,
+				"duration": 2,
+				"opacity": 8,
+				"speed": 3
+			},
+			"repulse": {
+				"distance": 73.08694910712106,
+				"duration": 0.4
+			},
+			"push": {
+				"particles_nb": 4
+			},
+			"remove": {
+				"particles_nb": 2
+			}
+		}
+	},
+	"retina_detect": true
+});
