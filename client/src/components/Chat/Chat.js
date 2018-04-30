5617aae7f4452463f43d479a7da1be464ceaf223
Chulps <chuckoward@gmail.com>
2018-04-30 00:02:45 -0700

    added more to chat element

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,25 +17,36 @@ class Chat extends React.Component{
                 <div className="row">
                     <div className="col-12 p-0">
                         <div>
-                            <div className="card-body h-50 p-0 m-0">
+                            <div className="card-body p-0 m-0">
                                 <div className="card-title"><h2>Global Chat</h2></div>
                                 <hr/>
-                                <div className="messages" placeholder="chat messages appear here">
+                                <div className="messages" >
+                                    <div className="row col-10 offset-1">
+                                        <small>
+                                            <ul>
+                                                <li>
+                                                    <h5>User Name:</h5>
+                                                    <span>This is a message</span>
+                                                </li>
+                                            </ul>
+                                        </small>
+                                    </div>
+
                                     {this.state.messages.map(message => {
                                         return (
-                                            <div><Userimage />{message.author}: {message.message}</div>
+                                            <div>{message.author}: {message.message}</div>
                                         )
                                     })}
                                 </div>
                                 <div className="footer m-0">
 
 
-                                {/* <input type="text" placeholder="Username" value={this.state.username} onChange={ev => this.setState({username: ev.target.value})} className="form-control"/>
-                                    <br/> */}
+                                <input type="text" placeholder="Username" value={this.state.username} onChange={ev => this.setState({username: ev.target.value})} className="form-control"/>
+                                    <br/>
                                     
                                     <input type="text" placeholder="Message" className="form-control" value={this.state.message} onChange={ev => this.setState({message: ev.target.value})}/>
                                     <br/>
-                                    <button className="btn btn-primary form-control">Send</button>
+                                    <button className="btn form-control">Send</button>
                                 </div>
                             </div>
                         </div>
