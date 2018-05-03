e5ca07088c9878c1422c70def51fece425449186
anthonyelshafei <anthony_elshafei@yahoo.com>
2018-05-03 11:32:17 -0700

    updated

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,18 +1,31 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Main from "../src/pages/Main"
 import Login from "../src/pages/Login";
 import SignUp from "../src/pages/SignUp";
-import Main from "../src/pages/Main"
 import NotFound from "../src/pages/NotFound";
+import API from "../src/utils/API";
 
 
 class Router extends React.Component {
+
+  state = {
+    userCount: []
+  }
+
+  componentDidMount(){
+    API.getUsers().then(res => {
+    this.setState({userCount: res.data})
+    })
+  };
+
 
   render () {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" render={Login} />
+          {/* <Route exact path="/" component={Login}/> */}
+          <Route exact path="/" render={() => <Login userCount={this.userCount} />} />
           <Route exact path="/signup" component={SignUp}/>
           <Route path="/main" component={Main}/>
           <Route component={NotFound}/>
