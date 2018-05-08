315cb5fe292e71f379438eae1335cf9ab8a018de
anthonyelshafei <anthony_elshafei@yahoo.com>
2018-05-07 22:19:31 -0700

    re-doing the image preview on signup page

--- client/src/pages/SignUp/SignUp.js
+++ client/src/pages/SignUp/SignUp.js
@@ -7,7 +7,7 @@ class SignUp extends React.Component {
     state = {
       username: "",
       password: "",
-      image: ""
+      image: "https://cdn.dribbble.com/users/149398/screenshots/1725558/fuego-loop-2.gif"
     }
 
     handleInputChange = event => {
@@ -55,10 +55,18 @@ class SignUp extends React.Component {
                 <form className="form" id="signup" onSubmit={this.handleFormSubmit}>
                     <input name="username" onChange={this.handleInputChange} value={this.state.username} type="text" placeholder="Username" required/>
                     <input name="password" onChange={this.handleInputChange} value={this.state.password} type="password" placeholder="Password" required/>
-                    <input id="imgName" name="image" onChange={this.handleInputChange} value={this.state.image} type="text" placeholder="Web Image URL"/>
+                    <input 
+                    id="imgName" 
+                    name="image" 
+                    onChange={this.handleInputChange} 
+                    value={this.state.image} 
+                    type="text" 
+                    placeholder="Web Image URL"
+                    required />
                     <button id="signup-button" className="grad1" type="submit">Sign Up</button><br/><br/>
                     <h3 id="linkz"><a href="/">Already a member?</a></h3>
                 </form>
+                <img src={this.state.image} name="imageHolder" id="imageHolder"/><br/><br/>
                 </div>
             </Fragment>
         )
