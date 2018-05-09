269c245b4b4f419227fc28bfaaee11075894b4c6
anthonyelshafei <anthony_elshafei@yahoo.com>
2018-05-09 13:08:09 -0700

    added an h2 tag on signup that says profile pic

--- client/src/pages/SignUp/SignUp.js
+++ client/src/pages/SignUp/SignUp.js
@@ -51,22 +51,23 @@ class SignUp extends React.Component {
         return (
             <Fragment>
                 <div className="container">
-                <Logo/>
-                <form className="form" id="signup" onSubmit={this.handleFormSubmit}>
-                    <input name="username" onChange={this.handleInputChange} value={this.state.username} type="text" placeholder="Username" required/>
-                    <input name="password" onChange={this.handleInputChange} value={this.state.password} type="password" placeholder="Password" required/>
-                    <input 
-                    id="imgName" 
-                    name="image" 
-                    onChange={this.handleInputChange} 
-                    value={this.state.image} 
-                    type="text" 
-                    placeholder="Web Image URL"
-                    required />
-                    <button id="signup-button" className="grad1" type="submit">Sign Up</button><br/><br/>
-                    <h3 id="linkz"><a href="/">Already a member?</a></h3>
-                </form>
-                <img src={this.state.image} name="imageHolder" id="imageHolder"/><br/><br/>
+                    <Logo/>
+                    <form className="form" id="signup" onSubmit={this.handleFormSubmit}>
+                        <input name="username" onChange={this.handleInputChange} value={this.state.username} type="text" placeholder="Username" required/>
+                        <input name="password" onChange={this.handleInputChange} value={this.state.password} type="password" placeholder="Password" required/>
+                        <input 
+                        id="imgName" 
+                        name="image" 
+                        onChange={this.handleInputChange} 
+                        value={this.state.image} 
+                        type="text" 
+                        placeholder="Web Image URL"
+                        required />
+                        <button id="signup-button" className="grad1" type="submit">Sign Up</button><br/><br/>
+                        <h3 id="linkz"><a href="/">Already a member?</a></h3>
+                    </form>
+                        <h2>Profile Pic</h2>
+                        <img src={this.state.image} name="imageHolder" id="imageHolder"/><br/><br/>
                 </div>
             </Fragment>
         )
